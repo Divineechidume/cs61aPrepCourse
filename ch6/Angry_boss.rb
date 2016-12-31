2dfe5750cc0f7b5db3a17f12f838a8f59e498e17
Divineechidume <divineechidume@berkeley.edu>
2016-12-30 18:30:50 -0800

    Complete chapter 6 'AFTtT' section

---
+++ ch6/Angry_boss.rb
@@ -0,0 +1,3 @@
+puts 'What do you want?'
+need = gets.chomp
+puts 'WHADDAYA MEAN ' + '"' + need.upcase + '"?!? YOU\'RE FIRED!!'
---
+++ ch6/Table_Of_Contents.rb
@@ -0,0 +1,6 @@
+line_width = 80
+puts('Table of Contents'.center(line_width/1.5))
+puts 
+puts('Chapter 1:   Getting Started'.ljust(line_width/2) + 'Page'.rjust(line_width/2) + '1'.rjust(line_width/25))
+puts('Chapter 2:   Numbers'.ljust(line_width/2) + 'Page'.rjust(line_width/2) + ' 9'.rjust(line_width/25))
+puts('Chapter 3:   Letters'.ljust(line_width/2) + 'Page'.rjust(line_width/2) + ' 13'.rjust(line_width/25))
