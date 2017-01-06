fa563570f5897c033902b29ed6bbc7ef4fef03ea
Divineechidume <divineechidume@berkeley.edu>
2017-01-05 17:20:19 -0800

    Update Bottles_of_Beer_on_the_wall.rb

--- ch7/Bottles_of_Beer_on_the_wall.rb
+++ ch7/Bottles_of_Beer_on_the_wall.rb
@@ -1,9 +1,17 @@
 bottles = 99
 while true
 	bottles = bottles - 1
-	puts bottles.to_s + ' bottles of beer on the wall, ' + bottles.to_s + ' bottles of beer.'
+	if bottles == 1
+		puts bottles.to_s + ' bottle of beer on the wall, ' + bottles.to_s + ' bottle of beer.'
+	else
+		puts bottles.to_s + ' bottles of beer on the wall, ' + bottles.to_s + ' bottles of beer.'
+	end
 	if bottles != 1
-		puts 'Take one down and pass it around, ' + (bottles - 1).to_s + ' bottles of Beer on the wall'
+		if bottles == 2
+			puts 'Take one down and pass it around, ' + (bottles - 1).to_s + ' bottle of Beer on the wall'
+		else
+			puts 'Take one down and pass it around, ' + (bottles - 1).to_s + ' bottles of Beer on the wall'
+		end
 	else
 		puts 'Take one down and pass it around, ' + ' no more bottles of Beer on the wall'
 		puts 'No more bottles of beer on the wall, no more bottles of beer.'
